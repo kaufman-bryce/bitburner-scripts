e21181920a97b06feeaf55767af2bf989f1822d3
kaufman-bryce <kaufman.bryce@gmail.com>
2022-02-21 11:12:15 -0700

    Ensure natmap() actually scans every loop

--- script/scan.js
+++ script/scan.js
@@ -3,13 +3,13 @@ export async function main(ns) {
   let servers = new Set();
 
   function netmap(targ = "home", depth = 0, parent = null) {
-    if (servers[targ] == undefined) {
+    // if (servers[targ] == undefined) {
       servers[targ] = ns.getServer(targ);
       servers[targ].parent = parent;
       servers[targ].depth = depth;
       servers[targ].children = ns.scan(targ).filter((child) => child != parent);
       servers[targ].children.forEach((child) => netmap(child, depth + 1, targ));
-    }
+    // }
   }
   do {
     netmap();
