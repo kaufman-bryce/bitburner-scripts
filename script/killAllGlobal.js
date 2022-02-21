20c15566b1ee5f5de6d87263d7d7889ba264edcf
kaufman-bryce <kaufman.bryce@gmail.com>
2022-02-21 16:27:36 -0700

    fixed incorrect argument call

--- script/killAllGlobal.js
+++ script/killAllGlobal.js
@@ -1,9 +1,8 @@
 export async function main(ns) {
   let servers = JSON.parse(ns.read("servers.txt")).servers;
-  const script = args[0];
+  const script = ns.args[0];
   for (const server in servers) {
-    const host = servers[server];
-    const processes = ns.ps(host.hostname);
+    const processes = ns.ps(server.hostname);
     for (let process of processes) {
       if (process.filename == script) {
         ns.kill(process.pid);
