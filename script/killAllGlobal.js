c3e094e629f2f91606c875f7bc6660e589167258
kaufman-bryce <kaufman.bryce@gmail.com>
2022-02-21 18:26:50 -0700

    Added killed process counter

--- script/killAllGlobal.js
+++ script/killAllGlobal.js
@@ -1,12 +1,16 @@
 export async function main(ns) {
   let servers = JSON.parse(ns.read("servers.txt")).servers;
   const script = ns.args[0];
+  let kills = 0;
   for (const server in servers) {
-    const processes = ns.ps(server.hostname);
+    let host = servers[server];
+    const processes = ns.ps(host.hostname);
     for (let process of processes) {
       if (process.filename == script) {
         ns.kill(process.pid);
+        kills++;
       }
     }
   }
+  ns.tprint(`INFO Killed ${kills} processes.`)
 }
